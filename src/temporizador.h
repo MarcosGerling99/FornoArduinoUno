6d36a0105e2d41535b5a1fe3ba97017e8a27811b
Marcos Gerling <sdgerling@gmai.com>
2021-07-25 22:00:04 -0300

    Bug

--- src/temporizador.h
+++ src/temporizador.h
@@ -39,7 +39,7 @@ void temporizador()
         // Serial.println(Offset);
         // Serial.print("botao:");
         // Serial.println(botao);
-        //Temperatura = ktc.readCelsius();
+        Temperatura = ktc.readCelsius();
         //Serial.print("Temperatura: ");   //IMPRIME O TEXTO NO MONITOR SERIAL
         //Serial.print(ktc.readCelsius()); //IMPRIME NO MONITOR SERIAL A TEMPERATURA MEDIDA
         //Serial.println("*C");            //IMPRIME O TEXTO NO MONITOR SERIAL
